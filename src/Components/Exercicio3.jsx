3c15fc3ad857245b2afc0e0ae1a52260888b61af
LeonardoRcmkt <leonardo@rcmkt.com.br>
2026-02-03 21:07:19 -0300

    exercise 3 melhora mais ainda e fazendo do 0

--- src/Components/Exercicio3.jsx
+++ src/Components/Exercicio3.jsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from "react";
 import { Button } from "./Button";
-
+import { useState } from "react";
 export const Exercicio3 = () => {
-  const increaseAmount = 5;
-
+  const increment = 5;
+  const [inc, setInc] = useState([]);
   const array = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
   ];
 
-  const [arrayVis, setArrayVis] = useState([]);
-
-  const reset = () => setArrayVis([]);
-
-  const increase = () => {
-    if (arrayVis.length >= array.length) return;
-    setArrayVis((prev) => [
+  const handleCount = () => {
+    if (inc.length > array.length) return;
+    setInc((prev) => [
       ...prev,
-      ...array.slice(prev.length, prev.length + increaseAmount),
+      ...array.slice(prev.length, prev.length + increment),
     ]);
   };
 
-  return (
-    <section className="section-home">
-      <Button onClick={increase}>Show more</Button>
-      <Button onClick={reset}>Reset</Button>
+  const handleReset = () => setInc([]);
 
-      {arrayVis.map(
-        (item, index) => item + (index < arrayVis.length - 1 && ", "),
-      )}
-    </section>
+  // console.log(inc);
+
+  return (
+    <div className="section-home">
+      <Button onClick={handleCount}>Increase</Button>
+      <Button onClick={handleReset}>Reset</Button>
+      {inc.map((item, index) => item + (index < inc.length - 1 && ", "))}
+    </div>
   );
 };
